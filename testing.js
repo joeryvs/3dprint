d1239e8d856a048c4c77cc6136c028a3a4607cba
Mister Coder <3dprintingwebstienl@gmail.com>
2022-03-15 15:35:31 +0100

    dinsdagWerk15:35

---
+++ testing.js
@@ -0,0 +1,19 @@
+window.onoffline = function() {
+    open("https://google.com");
+}
+
+let ll11 = document.getElementById(ll1);
+let ll22 = document.getElementById(ll2);
+let ll33= document.getElementById(ll3);
+let ll44 = document.getElementById(ll4);
+let ll55 = document.getElementById(ll5);
+let ll66 = document.getElementById(ll6);
+let ll77 = document.getElementById(ll7);
+
+var bur = document.getElementsByClassName("button");
+
+
+
+// ll11.onclick() = function () {
+//   console.log("ll1");
+// };
