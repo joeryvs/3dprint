f7d4f863c85c09669b34c15765af6a6630294d88
Mister Coder <3dprintingwebstienl@gmail.com>
2022-04-08 15:20:27 +0200

    final lap

--- DropDown.js
+++ DropDown.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
@@ -20,26 +19,3 @@ window.onclick = function (event) {
     }
   }
 };
-=======
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-
-var menu_open = (idd) => {
-  document.getElementById(idd).classList.toggle("show");
-};
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  // console.log("click");
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-
-    for (var i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
->>>>>>> ad6b4115474dd5b06c8f347e64586fb7c98f2845
